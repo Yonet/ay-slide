7f59cbf48f9dc430ed9b9cc9598a1dd2bdf88aed
Aysegul Yonet <aysegulyonet@gmail.com>
2017-02-13 21:55:58 -0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ay-slide',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
